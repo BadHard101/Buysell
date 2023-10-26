c399114c798b43d6ffd277d701f339f549f2d862
BadHard <BadHard101@yandex.ru>
2023-10-26 16:43:13 +0300

    1) phones.ftlh fixed 2) CRUD WashingMachine added

---
+++ src/main/java/com/example/rschir_buysell/controllers/products/WashingMachineController.java
@@ -0,0 +1,99 @@
+package com.example.rschir_buysell.controllers.products;
+
+import com.example.rschir_buysell.models.Client;
+import com.example.rschir_buysell.models.enums.ProductType;
+import com.example.rschir_buysell.models.products.WashingMachine;
+import com.example.rschir_buysell.services.products.WashingMachineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/washingMachine")
+public class WashingMachineController {
+    private final WashingMachineService washingMachineService;
+
+    @GetMapping("/selling")
+    public String showWashingMachines(Model model, Principal principal) {
+        List<WashingMachine> washingMachines = washingMachineService.getAllWashingMachines();
+        model.addAttribute("washingMachines", washingMachines);
+        model.addAttribute("user", washingMachineService.getClientByPrincipal(principal));
+        model.addAttribute("types", ProductType.values());
+        return "products/washingMachine/washingMachines";
+    }
+
+    @GetMapping("/create")
+    public String createWashingMachinePage(Model model, Principal principal) {
+        model.addAttribute("user", washingMachineService.getClientByPrincipal(principal));
+        return "products/washingMachine/washingMachineCreator";
+    }
+
+    @PostMapping("/create")
+    public String createWashingMachine(WashingMachine washingMachine, Principal principal, Model model) {
+        washingMachine.setProductType(ProductType.WashingMachines);
+        String st = washingMachineService.createWashingMachine(principal, washingMachine);
+        if (st.equals("Success")) {
+            return "redirect:/washingMachine/selling";
+        } else {
+            model.addAttribute("user", washingMachineService.getClientByPrincipal(principal));
+            model.addAttribute("errorMessage", st);
+            return "products/washingMachine/washingMachineCreator";
+        }
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editWashingMachineForm(@PathVariable("id") Long id, Model model, Principal principal) {
+        model.addAttribute("user", washingMachineService.getClientByPrincipal(principal));
+        WashingMachine washingMachine = washingMachineService.getWashingMachineById(id);
+        model.addAttribute("washingMachine", washingMachine);
+
+        double capacity = washingMachine.getTankCapacity();
+        String formattedCapacity = Double.toString(capacity).replace(" ", "");
+        model.addAttribute("formattedCapacity", formattedCapacity);
+
+        double price = washingMachine.getPrice();
+        String formattedPrice = Double.toString(price).replace(" ", "");
+        model.addAttribute("formattedPrice", formattedPrice);
+
+        return "products/washingMachine/washingMachineEditor";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateWashingMachine(@PathVariable("id") Long id, WashingMachine washingMachine, Model model, Principal principal) {
+        String st = washingMachineService.updateWashingMachine(id, washingMachine);
+        if (st.equals("Success")) {
+            return "redirect:/washingMachine/selling";
+        } else {
+            model.addAttribute("user", washingMachineService.getClientByPrincipal(principal));
+            model.addAttribute("washingMachine", washingMachine);
+
+            WashingMachine orig = washingMachineService.getWashingMachineById(id);
+
+            double capacity = orig.getTankCapacity();
+            String formattedCapacity = Double.toString(capacity).replace(" ", "");
+            model.addAttribute("formattedCapacity", formattedCapacity);
+
+            double price = orig.getPrice();
+            String formattedPrice = Double.toString(price).replace(" ", "");
+            model.addAttribute("formattedPrice", formattedPrice);
+
+            model.addAttribute("errorMessage", st);
+            return "products/washingMachine/washingMachineEditor";
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteWashingMachine(@PathVariable Long id, @AuthenticationPrincipal Client client) {
+        washingMachineService.deleteWashingMachine(id, client);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/rschir_buysell/services/products/WashingMachineService.java
@@ -0,0 +1,109 @@
+package com.example.rschir_buysell.services.products;
+
+import com.example.rschir_buysell.models.Client;
+import com.example.rschir_buysell.models.products.WashingMachine;
+import com.example.rschir_buysell.repositories.ClientRepository;
+import com.example.rschir_buysell.repositories.products.WashingMachineRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class WashingMachineService {
+    private final WashingMachineRepository washingMachineRepository;
+    private final ClientRepository clientRepository;
+
+    public Client getClientByPrincipal(Principal principal) {
+        if (principal == null) return new Client();
+        return clientRepository.findByEmail(principal.getName());
+    }
+
+    public List<WashingMachine> getAllWashingMachines() {
+        return washingMachineRepository.findAll();
+    }
+
+    public String createWashingMachine(Principal principal, WashingMachine washingMachine) {
+        if (
+                washingMachine.getPrice() != null &&
+                        washingMachine.getPrice() > 0 &&
+                        washingMachine.getName() != null &&
+                        !washingMachine.getName().isEmpty() &&
+                        // local variables
+                        washingMachine.getManufacturer() != null &&
+                        !washingMachine.getManufacturer().isEmpty() &&
+                        washingMachine.getTankCapacity() != null &&
+                        washingMachine.getTankCapacity() > 0
+        ) {
+            washingMachine.setClient(getClientByPrincipal(principal));
+            washingMachineRepository.save(washingMachine);
+        } else {
+            if (washingMachine.getPrice() == null) {
+                return "Укажите цену!";
+            } else if (washingMachine.getPrice() <= 0) {
+                return "Укажите корректную цену!";
+            } else if (washingMachine.getName() == null || washingMachine.getName().isEmpty()) {
+                return "Напишите название стиральной машины!";
+            } else if (washingMachine.getManufacturer() == null || washingMachine.getManufacturer().isEmpty()) {
+                return "Укажите производителя!";
+            } else if (washingMachine.getTankCapacity() == null) {
+                return "Укажите вместимость бака!";
+            } else if (washingMachine.getTankCapacity() < 0) {
+                return "Укажите корректную вместимость бака!";
+            }
+        }
+        return "Success";
+    }
+
+    public WashingMachine getWashingMachineById(Long id) {
+        return washingMachineRepository.getById(id);
+    }
+
+    public String updateWashingMachine(Long id, WashingMachine washingMachine) {
+        if (
+                washingMachine.getPrice() != null &&
+                        washingMachine.getPrice() > 0 &&
+                        washingMachine.getName() != null &&
+                        !washingMachine.getName().isEmpty() &&
+                        // local variables
+                        washingMachine.getManufacturer() != null &&
+                        !washingMachine.getManufacturer().isEmpty() &&
+                        washingMachine.getTankCapacity() != null &&
+                        washingMachine.getTankCapacity() >= 0
+        ) {
+            WashingMachine original = washingMachineRepository.getById(id);
+            original.setName(washingMachine.getName());
+            original.setPrice(washingMachine.getPrice());
+            // local variables
+            original.setManufacturer(washingMachine.getManufacturer());
+            original.setTankCapacity(washingMachine.getTankCapacity());
+            washingMachineRepository.save(original);
+        } else {
+            if (washingMachine.getPrice() == null) {
+                return "Укажите цену!";
+            } else if (washingMachine.getPrice() <= 0) {
+                return "Укажите корректную цену!";
+            } else if (washingMachine.getName() == null || washingMachine.getName().isEmpty()) {
+                return "Напишите название стиральной машины!";
+            } else if (washingMachine.getManufacturer() == null || washingMachine.getManufacturer().isEmpty()) {
+                return "Укажите производителя!";
+            } else if (washingMachine.getTankCapacity() == null) {
+                return "Укажите вместимость бака!";
+            } else if (washingMachine.getTankCapacity() < 0) {
+                return "Укажите корректную вместимость бака!";
+            }
+        }
+
+        return "Success";
+    }
+
+    public void deleteWashingMachine(Long id, Client client) {
+        if (client.getId() == washingMachineRepository.getById(id).getClient().getId()) {
+            washingMachineRepository.delete(washingMachineRepository.getById(id));
+        }
+    }
+}
