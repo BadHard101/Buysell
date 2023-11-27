1ac0e423198e709fb8feb778f757537f0f6b5920
BadHard <BadHard101@yandex.ru>
2023-11-27 03:41:20 +0300

    1) Users page for admin 2) Editing user for admin 3) + Product page

--- src/main/java/com/example/rschir_buysell/controllers/AdminController.java
+++ src/main/java/com/example/rschir_buysell/controllers/AdminController.java
@@ -1,19 +1,18 @@
 package com.example.rschir_buysell.controllers;
 
 import com.example.rschir_buysell.models.Client;
+import com.example.rschir_buysell.models.enums.Role;
 import com.example.rschir_buysell.models.products.Product;
 import com.example.rschir_buysell.repositories.ClientRepository;
 import com.example.rschir_buysell.repositories.products.ProductRepository;
+import com.example.rschir_buysell.services.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -21,21 +20,47 @@ import java.util.List;
 @RequestMapping("/admin")
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class AdminController {
-    private final ClientRepository clientRepository;
-    private final ProductRepository productRepository;
+    private final AdminService adminService;
 
-    /*@GetMapping("/panel")
-    public String showPanel() {
+    @GetMapping("/panel")
+    public String showPanel(@AuthenticationPrincipal Client client, Model model) {
+        model.addAttribute("user", client);
         return "admin/panel";
-    }*/
+    }
 
     @GetMapping("/user/{id}")
     public String showUserInfo(@PathVariable("id") Long id, Model model, @AuthenticationPrincipal Client user) {
-        Client client = clientRepository.getById(id);
-        List<Product> products = productRepository.findAll();
+        Client client = adminService.getClientById(id);
+        List<Product> products = adminService.findAllClientProducts(client);
+
         model.addAttribute("user", user);
         model.addAttribute("client", client);
         model.addAttribute("products", products);
         return "user/userPage";
+    }
+
+    @GetMapping("/users")
+    public String showUsers(@RequestParam(name = "email", required = false) String email, Model model) {
+        model.addAttribute("users", adminService.findAllClients(email));
+        return "admin/usersPanel";
+    }
+
+    @PostMapping("/user/ban/{id}")
+    public String userBan(@PathVariable("id") Long id) {
+        adminService.banUser(id);
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("/user/edit/{user}")
+    public String userEdit(@PathVariable("user") Client user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "admin/userEdit";
+    }
+
+    @PostMapping("/user/edit")
+    public String userEdit(@RequestParam("userId") Client user, @RequestParam("userRole") String role) {
+        adminService.changeUserRole(user, role);
+        return "redirect:/admin/users";
     }
 }
